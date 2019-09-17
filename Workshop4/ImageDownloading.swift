d8c47f74fa1105e12cfd556584bd3396300b018e
Kent Humphries <kent.humphries@gmail.com>
2019-09-17 15:52:21 +0200

    Did this add image caching maybe

--- Workshop4/ImageDownloading.swift
+++ Workshop4/ImageDownloading.swift
@@ -8,7 +8,13 @@ enum ImageDownloaderError: Error {
 }
 
 struct ImageDownloader {
-    private let session = URLSession.shared
+    private let session: URLSession
+    
+    init() {
+        let configuration = URLSession.shared.configuration
+        configuration.requestCachePolicy = .returnCacheDataElseLoad
+        session = URLSession(configuration: configuration)
+    }
 
     @discardableResult func downloadImage(url: URL, completion: @escaping (Result<UIImage, ImageDownloaderError>) -> Void) -> URLSessionDataTask {
         let task = session.dataTask(with: url) { data, response, error in
